c0b9182aba25887bdb8244b7dab577d946c0f98b
Eliav N <40385053+EliavN@users.noreply.github.com>
2019-07-14 00:32:36 +0300

    Create 2017A84-search.c

---
+++ 2017A84-search.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#define NOT_FOUND -1
+
+int search(void *array, void *var, int arrLength, int typeSize ) 
+{
+	int index = 0;
+	const unsigned char *ptr = array;
+	for(index = 0; index < arrLength; ptr += typeSize, index++)
+		if(memcmp(ptr, var, typeSize) == 0)
+			return index;
+	return NOT_FOUND;
+}
+
+int main(void)
+{
+	int intArray[] = {244,3,61,2,59,4,5,6,7};
+	int intNum = 59;
+	double doubleArray[] = {720.0,45.0,53.0,1,51.0,57.0,2,1,52.5};
+	double doubleNum = 52.5;
+
+	int result = search(intArray, &intNum, 9, sizeof(int));
+	if(result == NOT_FOUND)
+		printf("Cannot find the requested number in the given array\n");
+	else
+		printf("The number %d is founded in cell %d\n", intNum, result);
+
+	result = search(doubleArray, &doubleNum, 9, sizeof(double));
+	if(result == NOT_FOUND)
+		printf("Cannot find the requested number in the given array\n");
+	else
+		printf("The number %f is founded in cell %d\n", doubleNum, result);
+
+	return 0;
+}
