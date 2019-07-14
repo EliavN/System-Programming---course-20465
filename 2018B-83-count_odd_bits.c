64e3ffb6617e364f842434a065d4d20f47a895ff
Eliav N <40385053+EliavN@users.noreply.github.com>
2019-07-14 12:52:45 +0300

    Update 2018B-83-count_odd_bits.c

--- 2018B-83-count_odd_bits.c
+++ 2018B-83-count_odd_bits.c
@@ -2,16 +2,16 @@
 #include <limits.h>
 int count_odd_bits(void *num, int sizeOfType)
 {
-  int curByte = 0 , counter = 0;
-  unsigned char *ptr = num;
-  for(; curByte<sizeOfType; curByte++, ptr++)
-    for(*ptr>>=1; *ptr; counter+=(*ptr & 1), *ptr>>=2);
-  return counter;
+	int curByte = 0 , counter = 0;
+	unsigned char *ptr = num;
+	for(; curByte<sizeOfType; curByte++, ptr++)
+		for(*ptr>>=1; *ptr; counter+=(*ptr & 1), *ptr>>=2);
+	return counter;
 }
 
 int main()
 {
-  int num=149;
-  printf("counter is: %d\n",count_odd_bits(num));
-  return 0; 
+	int num=149;
+	printf("counter is: %d\n",count_odd_bits(num));
+	return 0; 
 }
